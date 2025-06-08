ff571de2a1cf3c9586e92187fa637003354f0a76
Semyen <sspukhov@yandex.ru>
2025-06-08 21:46:22 +0400

    fixes

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+
+console.log(123123, import.meta.env.VITE_API_URL)
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:58000/api',
 });
